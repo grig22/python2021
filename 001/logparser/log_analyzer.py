9e6475a01540f4ae1a31d74bac2ba5434931bb7d
Ваше Имя <you@example.com>
2022-01-04 10:23:11 +0300

    разные полезные дополнения

--- 001/logparser/log_analyzer.py
+++ 001/logparser/log_analyzer.py
@@ -5,21 +5,33 @@ import re
 import statistics
 import json
 import sys
+import os
+import datetime
+import collections
 
 
-def select_filename(conf):
-    # TODO честно выбирать из многих файлов
-    # TODO проверять время в имени файла и что есть ли для него уже отчёт
-    log_dir = conf['LOG_DIR']
-    return f'{log_dir}/nginx-access-ui.log-20170630.gz'
+def split_filenames(names):
+    regexp = r'nginx-access-ui\.log-(\d+).?(\S*)'
+    prog = re.compile(regexp)
+    Filename = collections.namedtuple('Filename', 'name date extension')
+    for name in names:
+        try:
+            result = prog.match(name)
+            dd = result.group(1)
+            date = datetime.date.fromisoformat(f'{dd[0:4]}-{dd[4:6]}-{dd[6:8]}')
+        except:
+            continue
+        else:
+            yield Filename(name=name, date=date, extension=result.group(2))
 
 
-def yield_lines(name):
-    ext = name.split('.')[-1]
-    opener = {
-        'gz': gzip.open,
-    }.get(ext, open)
-    with opener(name, mode='rt', encoding="utf-8") as fd:
+def get_last_log(log_dir):
+    return sorted(split_filenames(os.listdir(log_dir)), key=lambda x: x.date, reverse=True)[0]
+
+
+def yield_lines(filename, extension):
+    opener = {'gz': gzip.open}.get(extension, open)
+    with opener(filename, mode='rt', encoding="utf-8") as fd:
         for line in fd:
             yield line
 
@@ -53,13 +65,13 @@ def push(coll, data):
 # "time_perc": 9.0429999999999993, - суммарный $request_time для данного URL'а,
 #   в процентах относительно общего $request_time всех запросов
 # "count_perc": 0.106}  - сколько раз встречается URL, в процентах относительно общего числа запросов
-def stat(coll, conf):
+def stat(collector, report_size):
     overall = {
         'time': 0.0,
         'count': 0.0,
     }
-    for url in coll:
-        times = list(map(float, coll[url]))
+    for url in collector:
+        times = list(map(float, collector[url]))
         stats = {
             'url': url,
             'count': len(times),
@@ -70,31 +82,31 @@ def stat(coll, conf):
         }
         overall['time'] += stats['time_sum']
         overall['count'] += stats['count']
-        coll[url] = stats
+        collector[url] = stats
     # второй проход - когда посчитаны overall
-    for url in coll:
-        stats = coll[url]
+    for url in collector:
+        stats = collector[url]
         stats['time_perc'] = 100 * stats['time_sum'] / overall['time']
         stats['count_perc'] = 100 * stats['count'] / overall['count']
         # kinda post-processing
         for k, v in stats.items():
             if isinstance(v, float):
                 stats[k] = round(v, 3)
-    return sorted(list(coll.values()), key=lambda x: x['time_sum'], reverse=True)[:conf["REPORT_SIZE"]]
+    return sorted(list(collector.values()), key=lambda x: x['time_sum'], reverse=True)[:report_size]
 
 
-def save_report(data, conf):
-    template_filename = 'report.html'
-    rep_dir = conf['REPORT_DIR']
-    output_filename = f'{rep_dir}/report-2222.html'  # TODO filename
-    with open(template_filename, mode='rt', encoding="utf-8") as fi:
+def save_report(data, report_dir, report_fullname):
+    os.makedirs(report_dir, exist_ok=True)
+    with open('report.html', mode='rt', encoding="utf-8") as fi:
         body = fi.read().replace('$table_json', json.dumps(data, ensure_ascii=False))
-    with open(output_filename, mode='wt', encoding="utf-8") as fo:
+    with open(report_fullname, mode='wt', encoding="utf-8") as fo:
         fo.write(body)
 
 
 def apply_config(config, filename):
-    print(f'using config "{filename}"')
+    print(f'Используем файл конфигурации "{filename}"')
+    if os.stat(filename).st_size == 0:
+        return
     with open(filename, mode='rt', encoding="utf-8") as ff:
         conf_json = json.load(ff)
         for k in conf_json:
@@ -112,14 +124,21 @@ def main():
     try:
         if len(sys.argv) >= 2 and sys.argv[1] == '--config':
             apply_config(config, sys.argv[2])
-        print(f'config = {config}')
-        log_filename = select_filename(conf=config)
-        for data in parse(yield_lines(log_filename)):
+        print(f'Параметры конфигурации {config}')
+        log_dir = config['LOG_DIR']
+        report_dir = config['REPORT_DIR']
+        name, date, ext = get_last_log(log_dir)
+        report_fullname = f'{report_dir}/report-{date.isoformat().replace("-", ".")}.html'
+        if os.path.isfile(report_fullname):
+            raise Exception(f'Файл "{report_fullname}" уже существует, всё отменяется')
+        log_fullname = f'{log_dir}/{name}'
+        for data in parse(yield_lines(log_fullname, ext)):
             push(collector, data)
-        report = stat(coll=collector, conf=config)
-        save_report(data=report, conf=config)
+        report = stat(collector=collector, report_size=config['REPORT_SIZE'])
+        save_report(data=report, report_dir=report_dir, report_fullname=report_fullname)
     except Exception as ex:
-        print(f'ВОЗНИКЛО ИСКЛЮЧЕНИЕ: line {sys.exc_info()[2].tb_lineno} "{ex}"')
+        print(f'ВОЗНИКЛО ИСКЛЮЧЕНИЕ в строке {sys.exc_info()[2].tb_lineno}: {sys.exc_info()[0].__name__} "{ex}"')
+        raise
 
 
 if __name__ == "__main__":
