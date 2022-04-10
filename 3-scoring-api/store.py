bbf243fd1695d1478796f1ff7601b7c7c6a7ce03
Your Name <you@example.com>
2022-04-10 18:49:32 +0300

    interests

--- 3-scoring-api/store.py
+++ 3-scoring-api/store.py
@@ -10,43 +10,48 @@ def now():
 
 class Store:
     def __init__(self):
-        # self.score_cache = dict()
-        self.interests = dict() ######################
         self.MAX_TRY = 10
         self.red = None
 
     def getred(self):
-        if not self.red:
-            for i in range(self.MAX_TRY):
-                try:
-                    self.red = redis.Redis()
-                    if self.red.ping():
-                        return self.red
-                except:
-                    pass
-        return self.red
+        for i in range(self.MAX_TRY):
+            try:
+                self.red = redis.Redis()
+                if self.red.ping():
+                    return self.red
+            except Exception as ex:
+                pass
+        return None
 
     def cache_get(self, key):
-        if not self.getred() or not (score := self.getred().get(key)):
+        red = self.getred()
+        if not red or not (score := red.get(key)):
             return None
         score = pickle.loads(score)
         if score['created'] + score['ttl'] < now():
-            self.red.delete(key)
+            red.delete(key)
             return None
         return score['score']
 
     def cache_set(self, key, score, ttl):
-        if not self.getred():
+        red = self.getred()
+        if not red:
             return None
         val = {
             'score': score,
             'ttl': timedelta(seconds=ttl),
             'created': now(),
         }
-        self.red.set(key, pickle.dumps(val))
+        red.set(key, pickle.dumps(val))
 
     def get(self, cid):
-        if cid not in self.interests:
+        red = self.getred()
+        if not red:
+            return None
+        if not (inte := red.get(cid)):  # они не пересекаются
             interests = ["cars", "pets", "travel", "hi-tech", "sport", "music", "books", "tv", "cinema", "geek", "otus"]
-            self.interests[cid] = random.sample(interests, 2)
-        return self.interests[cid]
+            inte = random.sample(interests, 2)
+            red.set(cid, pickle.dumps(inte))
+        else:
+            inte = pickle.loads(inte)
+        return inte
