ad2e8efb4b43cb426b02af51e38b258924c398b6
Your Name <you@example.com>
2022-04-10 16:20:37 +0300

    getred

--- 3-scoring-api/store.py
+++ 3-scoring-api/store.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone, timedelta
 import random
+import redis
 
 
 def now():
@@ -7,26 +8,41 @@ def now():
 
 
 class Store:
-
     def __init__(self):
-        self.score_cache = dict()
-        self.interests = dict()
+        # self.score_cache = dict()
+        self.interests = dict() ########
+        self.MAX_TRY = 10
+        self.red = None
+
+    def getred(self):
+        if not self.red:
+            for i in range(self.MAX_TRY):
+                try:
+                    self.red = redis.Redis()
+                    ping = self.red.ping()
+                    if ping:
+                        return self.red
+                except:
+                    continue
+        return self.red
 
     def cache_get(self, key):
-        score = self.score_cache.get(key)
-        if not score:
+        if not self.getred() or not (score := self.getred().get(key)):
             return None
         if score['created'] + score['ttl'] < now():
-            self.score_cache.pop(key)
+            self.red.delete(key)
             return None
         return score['score']
 
     def cache_set(self, key, score, ttl):
-        self.score_cache[key] = {
+        if not self.getred():
+            return None
+        val = {
             'score': score,
             'ttl': timedelta(seconds=ttl),
             'created': now(),
         }
+        self.red.set(key, val)
 
     def get(self, cid):
         if cid not in self.interests:
