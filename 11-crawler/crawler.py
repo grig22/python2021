3d1f206a287c0654510f7e2e5a1726bee44cd2ee
Your Name <you@example.com>
2022-04-20 18:48:35 +0300

    try download_all

--- 11-crawler/crawler.py
+++ 11-crawler/crawler.py
@@ -12,8 +12,7 @@ DUMPDIR = 'pages/'
 
 def get_mainpage():
     req = requests.get(url=MAINPAGE)
-    html_doc = req.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(req.text, 'html.parser')
     title_soup = soup.find_all(attrs={'class': "titlelink"})  # <a class="titlelink" href="item?id=31093910">
     hrefs = map(lambda title: title.get('href'), title_soup)
     links = map(lambda href: MAINPAGE + href if href.startswith('item?id=') else href, hrefs)
@@ -31,21 +30,37 @@ def download_page(dirname, title_url):
         fd.write(text)
 
 
-def download_all(dirname: str, title_url: str, comment_links: list = None):
+def download_all(dirname: str, title_url: str, comments_urls: set):
     pathlib.Path(dirname).mkdir(parents=True, exist_ok=True)
     download_page(dirname, title_url)
+    for url in comments_urls:
+        download_page(dirname, url)
+
+
+def fetch_comments_urls(comments_page):
+    print('COMMENTS', comments_page)
+    req = requests.get(url=comments_page)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    # print(soup.prettify())
+    accum = set()
+    comments = soup.find_all(attrs={'class': "commtext"})
+    for comm in comments:
+        accum = accum | set(tag.get('href') for tag in comm.find_all(name='a'))
+    return accum
 
 
 def main():
-    for ff in get_mainpage():
-        title_url, comments_urls = ff
-        dirname = DUMPDIR + urllib.parse.quote(title_url, safe='')
+    for title_page, comments_page in get_mainpage():
+        dirname = DUMPDIR + urllib.parse.quote(title_page, safe='')
         if os.path.exists(dirname):
-            print(f'SKIP {title_url}')
+            print(f'SKIP {title_page}')
             continue
         else:
-            print(f'DOWNLOAD {title_url}')
-            download_all(dirname, title_url)
+            print(f'DOWNLOAD {title_page}')
+            comments_urls = fetch_comments_urls(comments_page)
+            for ii in comments_urls:
+                print(ii)
+            # download_all(dirname, title_page, comments_urls)
 
 
 if __name__ == '__main__':
